c61180eb073d915e879b48e209facb074393978e
Renata Abreu <renata89abreu@gmail.com>
2020-10-08 18:12:17 -0300

    rename blogController.js to postsController.js

--- desafio/src/routes/blogRoutes.js
+++ desafio/src/routes/blogRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const blogController = require('../controllers/blogController');
+const blogController = require('../controllers/postsController');
 
 /**
 @route GET posts
